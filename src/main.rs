13e2a94646a84b9358c13215ba157148792d85b2
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-21 18:13:37 +0300

    REPL

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,12 @@ fn main() {
 
     // Wait for user input
     let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim())
+
+    loop {
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
