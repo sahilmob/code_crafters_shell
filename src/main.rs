3c0a21563e4d5213f46e638838028cde20008281
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-21 18:21:41 +0300

    exit builtin

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+
+        if input.trim() == "exit 0" {
+            std::process::exit(0)
+        }
+
         println!("{}: command not found", input.trim());
     }
 }
