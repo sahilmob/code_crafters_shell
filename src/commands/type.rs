aaef05efbebb5508402efc7d517baa5471594bf5
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-22 10:28:43 +0300

    codecrafters submit [skip ci]

--- src/commands/type.rs
+++ src/commands/type.rs
@@ -1,4 +1,6 @@
 use crate::{echo, exit};
+use std::env;
+use std::path::Path;
 
 pub static TYPE: &str = "type";
 
@@ -20,6 +22,32 @@ macro_rules! typ {
     };
 }
 
+fn split_path(path: &str) -> Vec<String> {
+    path.split(":").map(String::from).collect()
+}
+
+fn find_exec_in_dir(p: &str) -> bool {
+    let path = Path::new(&p);
+    if path.exists() {
+        return true;
+    }
+
+    false
+}
+
+pub fn handle_executables(path: String, cmd: &str) -> String {
+    let exec_paths = split_path(&path);
+
+    for p in exec_paths {
+        let local_path = format!("{}/{}", p, cmd);
+        if find_exec_in_dir(&local_path) {
+            return format!("{} is {}", cmd, local_path);
+        }
+    }
+
+    format!("{} not found", cmd)
+}
+
 pub fn typ(args: Vec<&str>) {
     if args.is_empty() {
         println!("not found")
@@ -27,8 +55,15 @@ pub fn typ(args: Vec<&str>) {
 
     let hm = typ!(exit::TYPE, echo::TYPE, self::TYPE);
 
-    match hm.get(args.last().unwrap()) {
+    let cmd = args.last().unwrap();
+
+    match hm.get(cmd) {
         Some(v) => println!("{}", v),
-        None => println!("{}: not found", args.last().unwrap()),
+        None => match env::var("PATH") {
+            Ok(v) => {
+                println!("{}", handle_executables(v, cmd))
+            }
+            Err(_) => println!("{}: not found", cmd),
+        },
     };
 }
