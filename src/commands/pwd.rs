0b6e350249dde7019cd25a2f4860d309fc9f3370
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-22 18:04:48 +0300

    codecrafters submit [skip ci]

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -3,8 +3,11 @@ use std::process::Command;
 pub static TYPE: &str = "pwd";
 
 pub fn pwd() -> String {
-    format!(
-        "{}",
-        String::from_utf8(Command::new("pwd").output().unwrap().stdout).unwrap()
-    )
+    match Command::new("pwd").output() {
+        Ok(v) => match String::from_utf8(v.stdout) {
+            Ok(v) => v.trim().to_string(),
+            Err(e) => e.to_string(),
+        },
+        Err(e) => e.to_string(),
+    }
 }
