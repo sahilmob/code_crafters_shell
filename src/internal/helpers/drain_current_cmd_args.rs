5a0cf1affb981c75864c1b4d88376b0708bc6ab3
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-23 21:07:01 +0300

    codecrafters submit [skip ci]

--- src/internal/helpers/drain_current_cmd_args.rs
+++ src/internal/helpers/drain_current_cmd_args.rs
@@ -2,7 +2,10 @@ pub fn drain_current_cmd_args(args: &mut Vec<String>) -> Vec<String> {
     let mut cmd_args: Vec<String> = Vec::new();
 
     while !args.is_empty() && args[0] != "|" {
-        cmd_args.push(args.remove(0));
+        let arg = args.remove(0);
+        if !arg.trim().is_empty() {
+            cmd_args.push(arg);
+        }
     }
 
     cmd_args
