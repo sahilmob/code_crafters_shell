c37184ffc88fd1ff92479874ee00523de450357f
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-24 17:35:17 +0300

    codecrafters submit [skip ci]

--- src/internal/cmd_parser/cmd_parser.rs
+++ src/internal/cmd_parser/cmd_parser.rs
@@ -37,42 +37,22 @@ fn parse_between_double_quotes(input: &str) -> String {
 
 fn match_single_quote(input: &str, i: usize) -> bool {
     matcher!(SINGLE_QUOTE, input, i)
-    // let r = Regex::new(SINGLE_QUOTE).unwrap();
-    // let mut loc = r.capture_locations();
-    // r.captures_read_at(&mut loc, input, i);
-    // loc.get(0).unwrap_or((usize::MAX, usize::MAX)).0 == i
 }
 
 fn match_double_quote(input: &str, i: usize) -> bool {
     matcher!(DOUBLE_QUOTE, input, i)
-    // let r = Regex::new(DOUBLE_QUOTE).unwrap();
-    // let mut loc = r.capture_locations();
-    // r.captures_read_at(&mut loc, input, i);
-    // loc.get(0).unwrap_or((usize::MAX, usize::MAX)).0 == i
 }
 
 fn match_back_slash(input: &str, i: usize) -> bool {
     matcher!(BACK_SLASH, input, i)
-    // let r = Regex::new(BACK_SLASH).unwrap();
-    // let mut loc = r.capture_locations();
-    // r.captures_read_at(&mut loc, input, i);
-    // loc.get(0).unwrap_or((usize::MAX, usize::MAX)).0 == i
 }
 
 fn match_space(input: &str, i: usize) -> bool {
     matcher!(SPACE, input, i)
-    // let r = Regex::new(SPACE).unwrap();
-    // let mut loc = r.capture_locations();
-    // r.captures_read_at(&mut loc, input, i);
-    // loc.get(0).unwrap_or((usize::MAX, usize::MAX)).0 == i
 }
 
 fn match_cmd_segment(input: &str, i: usize) -> bool {
     matcher!(CMD_SEGMENT, input, i)
-    // let r = Regex::new(CMD_SEGMENT).unwrap();
-    // let mut loc = r.capture_locations();
-    // r.captures_read_at(&mut loc, input, i);
-    // loc.get(0).unwrap_or((usize::MAX, usize::MAX)).0 == i
 }
 
 pub fn parse(input: String) -> Vec<String> {
@@ -83,14 +63,6 @@ pub fn parse(input: String) -> Vec<String> {
     while i < input.len() {
         if match_cmd_segment(input, i) {
             let token = token_extractor!(CMD_SEGMENT, input, i);
-            // let token = Regex::new(CMD_SEGMENT)
-            //     .unwrap()
-            //     .captures(&input[i..])
-            //     .unwrap()
-            //     .get(0)
-            //     .unwrap()
-            //     .as_str()
-            //     .to_string();
 
             result.push(token.clone());
 
@@ -100,33 +72,16 @@ pub fn parse(input: String) -> Vec<String> {
             let token = token_extractor!(BETWEEN_DOUBLE_QUOTES, input, i);
             let parsed_token = parse_between_double_quotes(token.as_str());
 
-            // let token = Regex::new(BETWEEN_DOUBLE_QUOTES)
-            //     .unwrap()
-            //     .captures(&input[i..])
-            //     .unwrap()
-            //     .get(0)
-            //     .unwrap()
-            //     .as_str()
-            //     .to_string();
-
             if !parsed_token.is_empty() {
                 result.push(parsed_token);
                 i += eat(&token);
             }
+
             i += eat(DOUBLE_QUOTE);
         } else if match_single_quote(input, i) {
             i += eat(SINGLE_QUOTE);
 
             let token: String = token_extractor!(BETWEEN_SINGLE_QUOTES, input, i);
-
-            // let token = Regex::new(BETWEEN_SINGLE_QUOTES)
-            //     .unwrap()
-            //     .captures(&input[i..])
-            //     .unwrap()
-            //     .get(0)
-            //     .unwrap()
-            //     .as_str()
-            //     .to_string();
 
             if !token.is_empty() {
                 result.push(token.clone());
@@ -140,15 +95,6 @@ pub fn parse(input: String) -> Vec<String> {
             i += 1;
         } else if match_space(input, i) {
             let token: String = token_extractor!(SPACE, input, i);
-
-            // let token = Regex::new(SPACE)
-            //     .unwrap()
-            //     .captures(&input[i..])
-            //     .unwrap()
-            //     .get(0)
-            //     .unwrap()
-            //     .as_str()
-            //     .to_string();
 
             result.push(" ".to_string());
             i += eat(&token);
